4322f3c3a4b21d7aba13d2eb435badb2d061ffff
leesuyeeeong <1913839@sookmyung.ac.kr>
2022-10-04 21:53:02 +0900

    4. 덧셈 프로그램

--- main.c
+++ main.c
@@ -4,16 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char c;
-	int num = 0;
+	int sum = 0;
+	int x;
+	int i;
 	
-	printf("문자열을  입력하시오.");
-	while( (c = getchar() ) != '\n')//입력 문자가 개행문자가 나올때까지 반복 
-	{
-		if ( c>='0' && c <= '9')//입력된 글자 (c)가 숫자인가?
-			num = num + 1;//그렇다면 num에 하나를 센다. 
-	}		
-	printf("숫자의 개수는 %i개입니다.\n",num);//출력 
+	printf("정수를  입력하시오.");
+	scanf("%d", &x);
+	 
+	for(i=0;i<=x;i++)
+		sum = sum + i; 
 	
-	return 0;
+	printf("더하기의 결과는 %i입니다.\n",sum);//결과 
+	
+	return 0; 
 }
