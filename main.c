0c968072d6305811b2af0bf5b25c8025346ba5f0
leesuyeeeong <1913839@sookmyung.ac.kr>
2022-10-05 22:04:11 +0900

    6. 숫자 맞추기 게임

--- main.c
+++ main.c
@@ -3,34 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	int x,y;
-	char op;
-	int result; 
-	
-	printf("enter the calculation");
-	scanf("%d %c %d", &x, &op, &y);
-	
-	switch(op)
-	{
-		case '+':
-			result = x + y;
-			break;
-			
-		case '-':
-			result = x - y;
-			break;
-			
-		case '*':
-			result = x * y;
-			break;
-			
-		default :
-			result = x / y;
-			break;
-	}
-	
-	printf("=%i\n",result);//°á°ú 
-	
+int main(){
+	int i, num, key=59;
+			while(1){
+					printf("Guess a number : ");
+					scanf("%d", &num);		
+					if(num==key){			
+						printf("Congratulation! trials : \n");
+						break;		
+					}		
+					else if(num<key) printf("low!\n");		
+					else if(num>key) printf("high!\n");	
+					}
 	return 0; 
 }
